370486a49642a94a51b4c7e7d667ff61de659d94
J34nPC24 <jean.cotera@epn.edu.ec>
2022-08-24 13:30:20 -0500

    Limpiando codigo

--- ProyectoAjedrezEPN/src/ajedrez/partida/Tablero.java
+++ ProyectoAjedrezEPN/src/ajedrez/partida/Tablero.java
@@ -14,7 +14,162 @@ public class Tablero {
 
     public Tablero() {
         this.casillas = new Casilla[LARGO_TABLERO][ANCHO_TABLERO];
+        colocarPiezasEnElTablero();
+    }
 
+    public void hacerMovimiento(Movimiento movimiento) throws MovimientoInvalido {
+        try {
+            if (movimientoEsValido(movimiento)) {
+                if (movimientoEsUnaCaptura(movimiento)) {
+                    capturarPieza(movimiento);
+                } else {
+                    moverPieza(movimiento);
+                }
+            }
+        } catch (EnroqueCorto ec) {
+            hacerEnroqueCorto(movimiento);
+        } catch (EnroqueLargo el) {
+            hacerEnroqueLargo(movimiento);
+        } catch (CoronacionAvanzando ca) {
+            hacerCoronacionAvanzando(movimiento);
+        } catch (CoronacionCapturando cc) {
+            hacerCoronacionCapturando(movimiento);
+        }
+    }
+
+    private void hacerCoronacionCapturando(Movimiento movimiento) {
+    }
+
+    private void hacerCoronacionAvanzando(Movimiento movimiento) {
+    }
+
+    private void hacerEnroqueLargo(Movimiento movimiento) throws MovimientoInvalido {
+    }
+
+    private void capturarPieza(Movimiento movimiento) {
+        if (casillas[movimiento.getFilaInicial()][movimiento.getColumnaInicial()].getPieza() instanceof Peon) {
+            if (movimiento.getColumnaInicial() != movimiento.getColumnaFinal()) {
+
+            }
+        }
+    }
+
+    private boolean movimientoEsUnaCaptura(Movimiento movimiento) throws MovimientoInvalido {
+        if (casillas[movimiento.getFilaInicial()][movimiento.getColumnaInicial()].getPieza() instanceof Peon) {
+            return verSiElPeonRealizaUnaCaptura(movimiento);
+        }
+        return casillas[movimiento.getFilaFinal()][movimiento.getColumnaFinal()].hayPieza() && colorDePiezaDiferente(movimiento);
+    }
+
+
+
+    private boolean colorDePiezaDiferente(Movimiento movimiento) {
+        return casillas[movimiento.getFilaFinal()][movimiento.getColumnaFinal()].getPieza().getColor() != casillas[movimiento.getFilaInicial()][movimiento.getColumnaInicial()].getPieza().getColor();
+    }
+
+    private void hacerEnroqueCorto(Movimiento movimiento) throws MovimientoInvalido {
+        if(movimiento.getColorDeJugador() == ColorPiezas.BLANCAS){
+            ArrayList<int[]> listaACorroborar = new ArrayList<>();
+            listaACorroborar.add(new int[]{0,5});
+            listaACorroborar.add(new int[]{0,6});
+            listaACorroborar.add(new int[]{0,7});
+
+            for (int i = 0; i < listaACorroborar.size(); i++) {
+                if (casillas[listaACorroborar.get(i)[0]][listaACorroborar.get(i)[1]].hayPieza()) {
+                    throw new MovimientoInvalido("Existe una pieza entre la coordenada inicial y final");
+                }
+                if (Arrays.equals(listaACorroborar.get(i), movimiento.getCoordenadasFinales())) {
+                    break;
+                }
+            }
+        }
+
+    }
+
+
+    private boolean movimientoEsValido(Movimiento movimiento) throws EnroqueCorto, EnroqueLargo, CoronacionCapturando, CoronacionAvanzando, MovimientoInvalido {
+        System.out.println(movimiento);
+
+        if (!(hayPiezaParaMover(movimiento))) {
+            throw new MovimientoInvalido("No hay pieza en la coordenada inicial pendejo");
+        }
+        if (!(laPiezaQueQuiereMoverEsDeSuColor(movimiento))) {
+            throw new MovimientoInvalido("La pieza que quiere mover no es de su color");
+        }
+
+        ArrayList<ArrayList<int[]>> listaDeCoordenadas;
+        listaDeCoordenadas = casillas[movimiento.getFilaInicial()][movimiento.getColumnaInicial()].getPieza().obtenerListaDeCoordenadasPosibles(movimiento);
+        if (!(laPiezaPuedeIrAEsaCoordenada(movimiento, listaDeCoordenadas))) {
+            throw new MovimientoInvalido("La pieza no se puede mover a la coordenada ingresada");
+        }
+        for (int i = 0; i < listaDeCoordenadas.size(); i++) {
+            System.out.print("Lista#" + i + "{");
+            for (int j = 0; j < listaDeCoordenadas.get(i).size(); j++) {
+                System.out.print("" + j + Arrays.toString(listaDeCoordenadas.get(i).get(j)));
+            }
+            System.out.print("}\n");
+        }
+        for (int i = 0; i < listaDeCoordenadas.size(); i++) {
+            if (estaListaContineLaJugadaFinal(listaDeCoordenadas.get(i), movimiento.getCoordenadasFinales())) {
+                for (int j = 0; j < listaDeCoordenadas.get(i).size(); j++) {
+                    if (casillas[listaDeCoordenadas.get(i).get(j)[0]][listaDeCoordenadas.get(i).get(j)[1]].hayPieza()) {
+                        throw new MovimientoInvalido("Existe una pieza entre la coordenada inicial y final");
+                    }
+                    if (Arrays.equals(listaDeCoordenadas.get(i).get(j), movimiento.getCoordenadasFinales())) {
+                        break;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean laPiezaPuedeIrAEsaCoordenada(Movimiento movimiento, ArrayList<ArrayList<int[]>> listaDeCoordenadas) {
+        for (int i = 0; i < listaDeCoordenadas.size(); i++) {
+            if (estaListaContineLaJugadaFinal(listaDeCoordenadas.get(i), movimiento.getCoordenadasFinales())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hayPiezaParaMover(Movimiento movimiento) {
+        return casillas[movimiento.getFilaInicial()][movimiento.getColumnaInicial()].hayPieza();
+    }
+
+    private boolean laPiezaQueQuiereMoverEsDeSuColor(Movimiento movimiento) {
+        return movimiento.getColorDeJugador() == casillas[movimiento.getFilaInicial()][movimiento.getColumnaInicial()].getPieza().getColor();
+    }
+
+    private boolean estaListaContineLaJugadaFinal(ArrayList<int[]> ints, int[] coordenadasFinales) {
+        for (int i = 0; i < ints.size(); i++) {
+            if (ints.get(i)[0] == coordenadasFinales[0] && ints.get(i)[1] == coordenadasFinales[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean verSiElPeonRealizaUnaCaptura(Movimiento movimiento) throws MovimientoInvalido {
+        if (movimiento.getColumnaInicial() != movimiento.getColumnaFinal() && casillas[movimiento.getFilaFinal()][movimiento.getColumnaFinal()].hayPieza() && colorDePiezaDiferente(movimiento)) {
+            return true;
+        } else if (movimiento.getColumnaInicial() == movimiento.getColumnaFinal()) {
+            return false;
+        } else {
+            throw new MovimientoInvalido("Peon no puede capturar, porque no hay pieza");
+        }
+    }
+    private void moverPieza(Movimiento movimiento) {
+        casillas[movimiento.coordenadasFinales[0]][movimiento.coordenadasFinales[1]].setPieza(casillas[movimiento.coordenadasIniciales[0]][movimiento.coordenadasIniciales[1]].getPieza());
+        casillas[movimiento.getFilaInicial()][movimiento.getColumnaInicial()].quitarPieza();
+    }
+    private void dejarTableroVacio() {
+        for (int i=0;i<LARGO_TABLERO;i++){
+            for (int j =0; j<ANCHO_TABLERO;j++){
+                casillas[i][j] = new Casilla();
+            }
+        }
+    }
+    private void colocarPiezasEnElTablero() {
         casillas[0][0] = new Casilla(new Torre(ColorPiezas.BLANCAS));
         casillas[0][7] = new Casilla(new Torre(ColorPiezas.BLANCAS));
         casillas[7][0] = new Casilla(new Torre(ColorPiezas.NEGRAS));
@@ -43,7 +198,6 @@ public class Tablero {
             }
         }
     }
-
     @Override
     public String toString() {
         String tableroCompleto = "";
@@ -61,134 +215,6 @@ public class Tablero {
         }
 
         return tableroCompleto;
-    }
-
-    public void hacerMovimiento(Movimiento movimiento) throws MovimientoInvalido {
-
-        try {
-            if (movimientoEsValido(movimiento)) {
-                if (movimientoEsUnaCaptura(movimiento)) {
-                    hacerCaptura(movimiento);
-                } else {
-                    casillas[movimiento.coordenadasFinales[0]][movimiento.coordenadasFinales[1]].setPieza(casillas[movimiento.coordenadasIniciales[0]][movimiento.coordenadasIniciales[1]].getPieza());
-                    casillas[movimiento.getFilaInicial()][movimiento.getColumnaInicial()].quitarPieza();
-                }
-
-
-            }
-        } catch (EnroqueCorto ec) {
-            hacerEnroqueCorto(movimiento);
-        } catch (EnroqueLargo el) {
-            hacerEnroqueLargo(movimiento);
-        } catch (CoronacionAvanzando ca) {
-            hacerCoronacionAvanzando(movimiento);
-        } catch (CoronacionCapturando cc) {
-            hacerCoronacionCapturando(movimiento);
-        }
-
-    }
-
-    private void hacerCoronacionCapturando(Movimiento movimiento) {
-    }
-
-    private void hacerCoronacionAvanzando(Movimiento movimiento) {
-    }
-
-    private void hacerEnroqueLargo(Movimiento movimiento) throws MovimientoInvalido {
-    }
-
-    private void hacerCaptura(Movimiento movimiento) {
-        if (casillas[movimiento.getFilaInicial()][movimiento.getColumnaInicial()].getPieza() instanceof Peon) {
-            if (movimiento.getColumnaInicial() != movimiento.getColumnaFinal()) {
-
-            }
-        }
-    }
-
-    private boolean movimientoEsUnaCaptura(Movimiento movimiento) throws MovimientoInvalido {
-        if (casillas[movimiento.getFilaInicial()][movimiento.getColumnaInicial()].getPieza() instanceof Peon) {
-            if (movimiento.getColumnaInicial() != movimiento.getColumnaFinal() && casillas[movimiento.getFilaFinal()][movimiento.getColumnaFinal()].hayPieza() && colorDePiezaDiferente(movimiento)) {
-                return true;
-            } else if (movimiento.getColumnaInicial() == movimiento.getColumnaFinal()) {
-                return false;
-            } else {
-                throw new MovimientoInvalido("Peon no puede capturar, porque no hay pieza");
-            }
-        }
-        return casillas[movimiento.getFilaFinal()][movimiento.getColumnaFinal()].hayPieza() && colorDePiezaDiferente(movimiento);
-    }
-
-    private boolean colorDePiezaDiferente(Movimiento movimiento) {
-        return casillas[movimiento.getFilaFinal()][movimiento.getColumnaFinal()].getPieza().getColor() != casillas[movimiento.getFilaInicial()][movimiento.getColumnaInicial()].getPieza().getColor();
-    }
-
-    private void hacerEnroqueCorto(Movimiento movimiento) {
-    }
-
-
-    private boolean movimientoEsValido(Movimiento movimiento) throws EnroqueCorto, EnroqueLargo, CoronacionCapturando, CoronacionAvanzando, MovimientoInvalido {
-        System.out.println(movimiento);
-
-        if (!(hayPiezaParaMover(movimiento))) {
-            throw new MovimientoInvalido("No hay pieza en la coordenada inicial pendejo");
-        }
-        if (!(laPiezaQueQuiereMoverEsDeSuColor(movimiento))) {
-            throw new MovimientoInvalido("La pieza que quiere mover no es de su color");
-        }
-
-        ArrayList<ArrayList<int[]>> listaDeCoordenadas;
-        listaDeCoordenadas = casillas[movimiento.getFilaInicial()][movimiento.getColumnaInicial()].getPieza().obtenerListaDeCoordenadasPosibles(movimiento);
-        if (!(laPiezaPuedeIrAEsaCoordenada(movimiento, listaDeCoordenadas))){
-            throw new MovimientoInvalido("La pieza no se puede mover a la coordenada ingresada");
-        }
-        for (int i = 0; i < listaDeCoordenadas.size(); i++) {
-            System.out.print("Lista#" + i + "{");
-            for (int j = 0; j < listaDeCoordenadas.get(i).size(); j++) {
-                System.out.print("" + j + Arrays.toString(listaDeCoordenadas.get(i).get(j)));
-            }
-            System.out.print("}\n");
-        }
-        for (int i = 0; i < listaDeCoordenadas.size(); i++) {
-            if (estaListaContineLaJugadaFinal(listaDeCoordenadas.get(i), movimiento.getCoordenadasFinales())) {
-                for (int j = 0; j < listaDeCoordenadas.get(i).size(); j++) {
-                    if (casillas[listaDeCoordenadas.get(i).get(j)[0]][listaDeCoordenadas.get(i).get(j)[1]].hayPieza()) {
-                        throw new MovimientoInvalido("Existe una pieza entre la coordenada inicial y final");
-                    }
-                    if (Arrays.equals(listaDeCoordenadas.get(i).get(j), movimiento.getCoordenadasFinales())){
-                        break;
-                    }
-                }
-            }
-        }
-
-
-        return true;
-    }
-
-    private boolean laPiezaPuedeIrAEsaCoordenada(Movimiento movimiento, ArrayList<ArrayList<int[]>> listaDeCoordenadas) {
-        for (int i = 0; i < listaDeCoordenadas.size(); i++) {
-            if (estaListaContineLaJugadaFinal(listaDeCoordenadas.get(i), movimiento.getCoordenadasFinales())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean hayPiezaParaMover(Movimiento movimiento) {
-        return casillas[movimiento.getFilaInicial()][movimiento.getColumnaInicial()].hayPieza();
-    }
-
-    private boolean laPiezaQueQuiereMoverEsDeSuColor(Movimiento movimiento) {
-        return movimiento.getColorDeJugador() == casillas[movimiento.getFilaInicial()][movimiento.getColumnaInicial()].getPieza().getColor();
-    }
-
-    private boolean estaListaContineLaJugadaFinal(ArrayList<int[]> ints, int[] coordenadasFinales) {
-        for (int i = 0; i < ints.size(); i++) {
-            if (ints.get(i)[0] == coordenadasFinales[0] && ints.get(i)[1] == coordenadasFinales[1]) {
-                return true;
-            }
-        }
-        return false;
     }
 }
 
